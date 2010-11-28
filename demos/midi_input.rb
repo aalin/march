cdd52c91f75213964c2438f4993e291f66abd04a
Andreas Alin <andreas.alin@gmail.com>
2010-11-28 11:18:49 +0100

    Don't trigger notes if they're already on.

--- demos/midi_input.rb
+++ demos/midi_input.rb
@@ -28,10 +28,13 @@ class MidiInput
   def pick_midi_source(stdin, stdout)
     loop do
       line = stdout.gets
-      if line.match(/^Choose source:/)
+      case line
+      when /^Choose source:/
         print "Choose source: "
         $stdout.flush
         stdin.puts(gets)
+      when /^Using source (\d+)/
+        puts "Using source #$1"
         break
       else
         puts line
@@ -40,7 +43,32 @@ class MidiInput
   end
 end
 
-def play_closest_note(midi, event, mode, channel)
+class MidiNoteTrigger
+  def initialize(channel)
+    @channel = channel
+
+    @midi = MIDIator::Interface.new
+    @midi.autodetect_driver
+
+    @notes = Hash.new { |hash, key| hash[key] = 0 }
+  end
+
+  def note_on(note, velocity)
+    if @notes[note] == 0
+      @midi.driver.note_on(note, @channel, velocity)
+    end
+    @notes[note] += 1
+  end
+
+  def note_off(note)
+    @notes[note] -= 1
+    if @notes[note] == 0
+      @midi.driver.note_off(note, @channel)
+    end
+  end
+end
+
+def play_closest_note(midi_note_triggerer, event, mode)
   return unless event[0] == 144
 
   given_note = March::Note.new(event[1])
@@ -54,14 +82,14 @@ def play_closest_note(midi, event, mode, channel)
 
   if velocity.zero?
     puts "\e[31m%s\e[0m" % note.to_s
-    midi.driver.note_off(note.value, channel)
+    midi_note_triggerer.note_off(note.value)
   else
     puts "\e[32m%s\e[0m" % note.to_s
-    midi.driver.note_on(note.value, channel, velocity)
+    midi_note_triggerer.note_on(note.value, velocity)
   end
 end
 
-def play_if_in_mode(midi, event, mode, channel)
+def play_if_in_mode(midi_note_triggerer, event, mode)
   return unless event[0] == 144
 
   note = March::Note.new(event[1])
@@ -70,17 +98,17 @@ def play_if_in_mode(midi, event, mode, channel)
   if mode.include?(note)
     if velocity.zero?
       puts "\e[31m%s\e[0m" % note.to_s
-      midi.driver.note_off(note.value, channel)
+      midi_note_triggerer.note_off(note.value)
     else
       puts "\e[32m%s\e[0m" % note.to_s
-      midi.driver.note_on(note.value, channel, velocity)
+      midi_note_triggerer.note_on(note.value, velocity)
     end
   else
     puts "\e[33m%s\e[0m" % note.to_s
   end
 end
 
-def play_chord(midi, event, mode, channel)
+def play_chord(midi_note_triggerer, event, mode)
   return unless event[0] == 144
 
   chord_root = March::Note.new(event[1])
@@ -91,21 +119,18 @@ def play_chord(midi, event, mode, channel)
   puts "\e[%dm%s\e[0m" % [velocity.zero? ? 31 : 32, notes.join(", ")]
 
   if velocity.zero?
-    notes.each { |note| midi.driver.note_off(note.value, channel) }
+    notes.each { |note| midi_note_triggerer.note_off(note.value) }
   else
-    notes.each { |note| midi.driver.note_on(note.value, channel, velocity + rand(velocity / 10)) }
+    notes.each { |note| midi_note_triggerer.note_on(note.value, velocity + rand(velocity / 10)) }
   end
 end
 
-mode = March::Mode.new(March::Note.from_name('C'), March::Scale.phrygian)
+mode = March::Mode.new(March::Note.from_name('C'), March::Scale.major)
 
-midi = MIDIator::Interface.new
-midi.autodetect_driver
-
-channel = 0
+midi_note_triggerer = MidiNoteTrigger.new(0)
 
 MidiInput.new.run do |event|
-  play_closest_note(midi, event, mode, channel) # Useful for not playing the "wrong" notes
-  # play_if_in_mode(midi, event, mode, channel) # Useful for practicing scales
-  # play_chord(midi, event, mode, channel)
+  play_closest_note(midi_note_triggerer, event, mode) # Useful for not playing the "wrong" notes
+  # play_if_in_mode(midi_note_triggerer, event, mode) # Useful for practicing scales
+  # play_chord(midi_note_triggerer, event, mode)
 end
